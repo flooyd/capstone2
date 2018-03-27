97c9669200eaf244e4818205b72e77acf362c65f
flooyd <floydtjones@gmail.com>
2018-03-27 22:08:53 +0000

    Finish login/register switcher

--- public/js/index.js
+++ public/js/index.js
@@ -16,23 +16,23 @@ $(() => {
   function handleLoginSubmit() {
     $('.registerLink').click(e => {
       let linkText = $(e.currentTarget).text();
-      if(linkText == 'Register') {
+      if(linkText == 'Login') {
         console.log('hi');
-        $('.loginForm p').text('Already registered? <span class="registerLink">Login</span>')
+        $(e.currentTarget).text('Register');
+        $('.currentForm-js').text('Need an account?')
+        $('.loginForm legend').text('Login');
+        $('.loginForm').attr('action', 'api/users');
+        $('.passwordConfirm').remove();
+      } else {
         $(e.currentTarget).text('Login');
-        $('.loginForm').attr('action', 'api/auth/login');
-        $('.loginForm button').text('Register');
+        $('.currentForm-js').text('Already Registered?')
+        $('.loginForm legend').text('Create Account');
+        $('.loginForm').attr('action', 'api/users');
         $('.loginForm button').before(
           `<div class="passwordConfirm">
           <label for="passwordConfirm">Confirm</label>
           <input type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Confirm password" required>
         </div>`)
-      } else {
-        $(e.currentTarget).text('Register');
-        $('.loginForm legend').text('Register');
-        $('.loginForm').attr('action', 'api/users');
-        $('.loginForm button').text('Login');
-        $('.passwordConfirm').remove();
       }
       
      
